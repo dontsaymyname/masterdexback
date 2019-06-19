7408d90c4c54bfbb78e7ba9e06e7b17ac9f3c7d8
dontsaymyname <michaelpinheiro00@gmail.com>
2019-06-18 23:03:50 -0300

    ok

--- app/src/main/java/com/example/masterdex/EsqueciASenha.java
+++ app/src/main/java/com/example/masterdex/EsqueciASenha.java
@@ -1,35 +1,57 @@
 package com.example.masterdex;
 
+import android.content.Intent;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class EsqueciASenha extends AppCompatActivity {
 
-    TextInputEditText emailDigitado;
+    private TextInputEditText emailDigitado;
+    private ImageView buttonVoltar;
+    private Button buttonEnviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_esqueci_asenha);
 
-        emailDigitado = findViewById(R.id.email_textinput);
 
-        Button buttonEnviar = findViewById(R.id.enviar_button);
+        initComponents();
+
+
+
+
         buttonEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 enviarClicado(view);
             }
         });
+
+        buttonVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                voltarParaLogin();
+            }
+        });
+
+
     }
 
 
 
+    private void initComponents() {
 
+        emailDigitado = findViewById(R.id.email_textinput);
+        buttonVoltar = findViewById(R.id.botao_voltar_esqueci_senha);
+        buttonEnviar = findViewById(R.id.enviar_button);
+
+    }
 
 
     public void enviarClicado (View view){
@@ -45,15 +67,18 @@ public class EsqueciASenha extends AppCompatActivity {
         if (emailDigitado.getText().toString().equals("")){
             //Se não houver texto no campo do email, ele irá exibir o erro abaixo
 
-            emailDigitado.setError("Digite um email");
+            emailDigitado.setError("Digite seu email");
 
         } else {
             //Caso o campo de email tenha algo, aparecerá um snackbar dizendo que o usuário foi cadastrado
+            //Clicar no "ok" do snackbar leva de volta para a tela de login
 
             Snackbar.make(view, "Email enviado", Snackbar.LENGTH_INDEFINITE)
                     .setAction("OK", new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
+
+                            voltarParaLogin();
 
                         }
                     }).setActionTextColor(getResources().getColor(R.color.branco)).show();
@@ -63,5 +88,11 @@ public class EsqueciASenha extends AppCompatActivity {
 
 
 
+    }
+
+    public void voltarParaLogin(){
+        //Ao clicar na seta de voltar ele irá para a tela de login
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
